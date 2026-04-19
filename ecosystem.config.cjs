c572fa7a818f0ab310635c283af964fb8f732a46
Gumballxnz <ghostgumball39@gmail.com>
2026-04-19 15:39:55 +0200

    Perf: RAM Plus (4GB) habilitada e otimização de detecção AutoDL.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
     name: 'senna-bot',
     script: 'index.js',
     cwd: '/home/ubuntu/senna-bot',
-    node_args: '--max-old-space-size=200 --expose-gc',
-    max_memory_restart: '200M',
+    node_args: '--max-old-space-size=4096 --expose-gc',
+    max_memory_restart: '4G',
     autorestart: true,
     watch: false,
-    kill_timeout: 3000,
+    kill_timeout: 10000,
     env: {
       NODE_ENV: 'production',
       TZ: 'Africa/Maputo'
