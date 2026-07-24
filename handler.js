32cd0ab5194ee76abf7a6da5b8a541eb5fbcfb9d
Gumballxnz <fbilionario01@gmail.com>
2026-07-24 07:47:20 +0200

    debug: log full raw WebMessageInfo object

--- handler.js
+++ handler.js
@@ -35,9 +35,7 @@ export async function handler(chatUpdate) {
     // Confirmação de Leitura Automática (Tique Azul)
     if (chatUpdate.messages) {
         for (const message of chatUpdate.messages) {
-            if (message.message) {
-                console.log('[RAW MSG RECEIVED]: keys =', Object.keys(message.message), '| id =', message.key?.id)
-            }
+            console.log('[RAW WEBMESSAGEINFO]:', JSON.stringify({ key: message.key, messageKeys: Object.keys(message.message || {}), m: message }))
             if (message.key && !message.key.fromMe) {
                 try {
                     if (typeof this.readMessages === 'function') {
